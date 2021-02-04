31d968e0fc44326df4c798e6dfaceb96c133f840
Pedro Bazzo <developerpedropbazzo@gmail.com>
2021-02-04 00:53:26 -0300

    gatsby

---
+++ site/.cache/sync-requires.js
@@ -0,0 +1,18 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/pedropbazzo/Downloads/pedropbazzo-blog-gatsby/site/.cache/dev-404-page.js"))),
+  "component---node-modules-gatsby-theme-elemental-src-pages-404-js": hot(preferDefault(require("/home/pedropbazzo/Downloads/pedropbazzo-blog-gatsby/node_modules/gatsby-theme-elemental/src/pages/404.js"))),
+  "component---node-modules-gatsby-theme-elemental-src-pages-contact-js": hot(preferDefault(require("/home/pedropbazzo/Downloads/pedropbazzo-blog-gatsby/node_modules/gatsby-theme-elemental/src/pages/contact.js"))),
+  "component---node-modules-gatsby-theme-elemental-src-pages-index-js": hot(preferDefault(require("/home/pedropbazzo/Downloads/pedropbazzo-blog-gatsby/node_modules/gatsby-theme-elemental/src/pages/index.js"))),
+  "component---node-modules-gatsby-theme-elemental-src-templates-basepage-js": hot(preferDefault(require("/home/pedropbazzo/Downloads/pedropbazzo-blog-gatsby/node_modules/gatsby-theme-elemental/src/templates/basepage.js"))),
+  "component---node-modules-gatsby-theme-elemental-src-templates-blog-js": hot(preferDefault(require("/home/pedropbazzo/Downloads/pedropbazzo-blog-gatsby/node_modules/gatsby-theme-elemental/src/templates/blog.js"))),
+  "component---node-modules-gatsby-theme-elemental-src-templates-blog-list-js": hot(preferDefault(require("/home/pedropbazzo/Downloads/pedropbazzo-blog-gatsby/node_modules/gatsby-theme-elemental/src/templates/blog-list.js"))),
+  "component---node-modules-gatsby-theme-elemental-src-templates-portfolio-js": hot(preferDefault(require("/home/pedropbazzo/Downloads/pedropbazzo-blog-gatsby/node_modules/gatsby-theme-elemental/src/templates/portfolio.js"))),
+  "component---node-modules-gatsby-theme-elemental-src-templates-portfolio-list-js": hot(preferDefault(require("/home/pedropbazzo/Downloads/pedropbazzo-blog-gatsby/node_modules/gatsby-theme-elemental/src/templates/portfolio-list.js")))
+}
+
---
+++ site/gatsby-config.js
@@ -0,0 +1,79 @@
+let siteMetadata = {
+  title: `Pedro Bazzo`,
+  capitalizeTitleOnHome: true,
+  logo: `/images/logo.png`,
+  icon: `/images/icon.png`,
+  titleImage: `/images/wall.jpg`,
+  introTag: ` Tech Lead Front | Senior Software Engineer Front End | MBA in Front End Development`,
+  description: `I work daily on creating new features for the Android and IOS application with React Native and the Web System with ReactJS and developing back-end with NodeJS, creating and maintaining microservices.`,
+  author: `@pedropbazzo`,
+  blogItemsPerPage: 20,
+  portfolioItemsPerPage: 20,
+  darkmode: true,
+  switchTheme: true,
+  navLinks: [
+    {
+      name: "HOME",
+      url: "/",
+    },
+    {
+      name: "ABOUT",
+      url: "/about",
+    },
+    {
+      name: "BLOG",
+      url: "/blog",
+    },
+    {
+      name: "PORTFOLIO",
+      url: "/portfolio",
+    },
+    {
+      name: "CONTACT",
+      url: "/contact",
+    },
+  ],
+  footerLinks: [
+    {
+      name: "PRIVACY POLICY",
+      url: "/privacy-policy",
+    },
+  ],
+  social: [
+    {
+      name: "Facebook",
+      icon: "/images/Facebook.svg",
+      url: "https://www.facebook.com/ispeter.paul/",
+    },
+    {
+      name: "Twitter",
+      icon: "/images/Twitter.svg",
+      url: "https://twitter.com/pedropbazzo",
+    },
+    {
+      name: "Instagram",
+      icon: "/images/Instagram.svg",
+      url: "https://www.instagram.com/pedropbazzo/",
+    },
+  ],
+  contact: {
+    api_url: "./test.json",
+    /* Leave this completely empty (no space either) if you don't want a contact form. */
+    description: `If you want to contact me for work purposes or if you have any questions send me an email. `,
+    mail: "developerpedropbazzo@gmail.com",
+    phone: "+55 15 998088356",
+    address: "+55 \nSão Paulo",
+  },
+};
+
+module.exports = {
+  siteMetadata: siteMetadata,
+  plugins: [
+    {
+      resolve: "gatsby-theme-elemental",
+      options: {
+        contentPath: "contents",
+      },
+    },
+  ],
+};
