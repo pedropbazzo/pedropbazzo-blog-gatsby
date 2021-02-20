1f72999b3eccb631f219e78b28017c1768b0652d
pedropbazzo <pedropbazzo@outlook.com>
2021-02-20 02:30:58 -0300

    fix: clean cash

--- site/.cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../../node_modules/gatsby-remark-images/gatsby-browser.js'),
-      options: {"plugins":[],"maxWidth":1280,"linkImagesToOriginal":true,"showCaptions":false,"markdownCaptions":false,"sizeByPixelDensity":false,"backgroundColor":"white","quality":50,"withWebp":false,"tracedSVG":false,"loading":"lazy","disableBgImageOnAlpha":false,"disableBgImage":false},
-    },{
-      plugin: require('../../node_modules/gatsby-theme-elemental/gatsby-browser.js'),
-      options: {"plugins":[],"contentPath":"contents"},
-    }]
